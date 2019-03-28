6b99bfc40013116b8d2bd2f5867147d73bfa5cd0
ivancho277 <rouskoi@live.com>
2019-03-27 21:54:40 -0700

    framework made

--- liri.js
+++ liri.js
@@ -2,3 +2,23 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var term = process.argv.splice(3).join(" ");
+
+switch(command){
+    case("concert-this"):
+
+    break;
+    case("spotify-this-song"):
+
+    break;
+    case("movie-this"):
+
+    break;
+    case("do-what-it-says"):
+
+    break;
+    default:
+    break;
+}
