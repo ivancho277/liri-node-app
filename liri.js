1483e3ae0b56a7bbdfdcd9d6f7c72beb5dd2f683
ivancho277 <rouskoi@live.com>
2019-03-27 22:33:14 -0700

    first part working

--- liri.js
+++ liri.js
@@ -1,13 +1,28 @@
 require("dotenv").config();
+var axios = require("axios");
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
-var term = process.argv.splice(3).join(" ");
+var term = process.argv.splice(3).join("");
+console.log(term);
+
+
 
 switch(command){
     case("concert-this"):
+        axios.get("https://rest.bandsintown.com/artists/"+term+"/events?app_id=codingbootcamp").then((response) => {
+            try{
+                //console.log(response)
+             console.log("Venue Name: "+response.data[0].venue.name);
+             console.log("City: " + response.data[0].venue.city +", "+response.data[0].venue.country);
+             console.log("Time: " + response.data[0].datetime);
+            }
+            catch(err){
+                throw err;
+            }
+        })
 
     break;
     case("spotify-this-song"):
